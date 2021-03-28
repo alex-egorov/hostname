e11f43796fe7b7cd9b38aea6bf72ba74f9205b24
pistacks <54232559+pistacks@users.noreply.github.com>
2021-03-28 14:51:30 +0200

    add health checks

--- app.go
+++ app.go
@@ -11,9 +11,21 @@ func hostnameHandler(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintln(w, "Hostname:", myhostname)
 }
 
+func healtcheckHandler(w http.ResponseWriter, r *http.Request) {
+    w.WriteHeader(200)
+    w.Write([]byte("ok"))
+}
+
+func fivehunderHandler(w http.ResponseWriter, r *http.Request) {
+    w.WriteHeader(500)
+    w.Write([]byte("error"))
+}
+
 func main() {
     const port string = "8000"
     fmt.Println("Server listening on port", port)
     http.HandleFunc("/", hostnameHandler)
+    http.HandleFunc("/health", healtcheckHandler)
+    http.HandleFunc("/bad", fivehunderHandler)
     http.ListenAndServe(":" + port, nil)
 }
